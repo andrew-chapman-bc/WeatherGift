136d8686e8d02ef1acc8515e2d6a22a3fa28be73
Andrew Chapman <andrew.chapman@bc.edu>
2021-10-11 20:50:22 -0400

    6.2 Initial View

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Andrew Chapman on 10/11/21.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
