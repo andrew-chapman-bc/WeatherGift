ed29d6955689bbd2e41abb9161cfc3e2d41cdd79
Andrew Chapman <andrew.chapman@bc.edu>
2021-10-24 23:48:09 -0400

    6.17 collection view cells

---
+++ WeatherGift/HourlyCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HourlyCollectionViewCell.swift
+//  WeatherGift
+//
+//  Created by Andrew Chapman on 10/24/21.
+//
+
+import UIKit
+
+class HourlyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var hourlyLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var hourlyTemperature: UILabel!
+    
+    
+    var hourlyWeather: HourlyWeather! {
+        didSet {
+            hourlyLabel.text = hourlyWeather.hour
+            iconImageView.image = UIImage(systemName: hourlyWeather.hourlyIcon)
+            hourlyTemperature.text = "\(hourlyWeather.hourlyTemperature)°"
+        }
+    }
+}
