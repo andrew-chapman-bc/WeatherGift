316b0b13a6046d95aec8072e9d56d1ff542416bb
Andrew Chapman <andrew.chapman@bc.edu>
2021-10-24 22:07:46 -0400

    6.16 hourly data

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 private let dateFormatter: DateFormatter = {
-    print("JUST CREATED A DATEFORMATTER")
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "EEEE, MMM d"
     return dateFormatter
